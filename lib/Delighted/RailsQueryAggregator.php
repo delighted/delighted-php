e026817a3aa0e6adfe021e862293449b6dfe9722
David Sklar <david.sklar@gmail.com>
2015-03-02 10:57:52 -0500

    Query aggregator, request exception

---
+++ lib/Delighted/RailsQueryAggregator.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Delighted;
+
+/**
+ * Aggregate nested query string values using Rails style:
+ * [] with no indicies for arrays, [] with indicies for hashes
+ */
+class RailsQueryAggregator extends \Guzzle\Http\QueryAggregator\PhpAggregator implements \Guzzle\Http\QueryAggregator\QueryAggregatorInterface {
+    public function aggregate($key, $value, \Guzzle\Http\QueryString $query) {
+        $ret = array();
+        if ($this->isNumericArray($value)) {
+            $ret = $this->numericAggregate($key, $value, $query);
+        } else {
+            /* For non-numeric arrays, treat them like hashes */
+            $ret = parent::aggregate($key, $value, $query);
+        }
+        return $ret;
+    }
+
+    protected function isNumericArray($ar) {
+        if (! (is_array($ar) && (count($ar) > 0))) {
+            return false;
+        }
+        $keys = array_keys($ar);
+        $firstKey = reset($keys);
+        $lastKey = end($keys);
+        /* Just check first and last, this should be fine except for
+         * pathologically broken cases. */
+        return (($firstKey === 0) && ($lastKey === (count($ar) - 1)));
+    }
+
+    protected function numericAggregate($key, $value, \Guzzle\Http\QueryString $query) {
+        $subKey = "{$key}[]";
+        $values = array();
+        foreach ($value as $k => $v) {
+            if (is_array($v)) {
+                $values[] = $this->aggregate($subKey, $v, $query);
+            } else {
+                $values[] = $v;
+            }
+        }
+        return array(rawurlencode($subKey) => $values);
+    }
+}
---
+++ lib/Delighted/RequestException.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Delighted;
+
+class RequestException extends \Exception {
+
+    protected $data;
+    protected $response;
+
+    public function __construct($code, $data = array(), \Exception $e) {
+        parent::__construct($e->getResponse()->getReasonPhrase(), $code, $e);
+        $this->data = $data;
+        $this->response = $e->getResponse();
+    }
+
+    public function getData() {
+        return $this->data;
+    }
+
+    public function getResponse() {
+        return $this->response;
+    }
+
+}
