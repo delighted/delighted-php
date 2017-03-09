fe36ab00984062fc4dd4b7a00085b65bc7bdb727
Avtandil Kikabidze <akalongman@gmail.com>
2017-03-09 13:54:55 +0400

    Add test for query string to ruby style converter

--- test/ClientTest.php
+++ test/ClientTest.php
@@ -24,4 +24,16 @@ class ClientTest extends Delighted\TestCase
         $client = Delighted\Client::getInstance(['apiKey' => '123abc']);
         $this->assertInstanceOf('Delighted\\Client', $client);
     }
+
+    public function testAggregate()
+    {
+        $value = [
+            'num'   => ['a', 'b', 'c'],
+            'assoc' => ['a' => 'Ann', 'b' => 'Bob'],
+        ];
+
+        $query_string = $this->client->convertQueryStringToRubyStyle($value);
+
+        $this->assertEquals('num[]=a&num[]=b&num[]=c&assoc[a]=Ann&assoc[b]=Bob', $query_string);
+    }
 }
